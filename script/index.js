501bf45935481cc6d5a00bfd86f71401d387bec5
Moreaux19 <basargin19@gmail.com>
2023-06-22 14:44:08 +0300

    JavaScript section redone after review

--- script/index.js
+++ script/index.js
@@ -1,27 +1,19 @@
 const editPopupButton = document.querySelector('.profile__edit-button');
-const editPopup = document.querySelector('.popup');
-const closePopupButton = document.querySelector('.popup__close-button');
-const savePopupButton = document.querySelector('.popup__save-button');
 const nameProfile = document.querySelector('.profile__name');
 const descriptionProfile = document.querySelector('.profile__description');
-const nameInput = document.querySelector('#name-input');
-const descriptionInput = document.querySelector('#description-input');
-const popupForm = document.querySelector('.popup__form');
-
-function openPopup(popup) {
-  popup.classList.add('popup_opened');
-}
-
-function closePopup(popup) {
-  popup.classList.remove('popup_opened');
-}
+const editPopup = document.querySelector('.popup');
+const closePopupButton = editPopup.querySelector('.popup__close-button');
+const popupForm = editPopup.querySelector('.popup__form');
+const nameInput = popupForm.querySelector('#name-input');
+const descriptionInput = popupForm.querySelector('#description-input');
+const savePopupButton = popupForm.querySelector('.popup__save-button');
 
 editPopupButton.addEventListener('click', function () {
-  openPopup(editPopup);
+  editPopup.classList.add('popup_opened');
 });
 
 closePopupButton.addEventListener('click', function () {
-  closePopup(editPopup);
+  editPopup.classList.remove('popup_opened');
 });
 
 nameInput.value = nameProfile.textContent;
@@ -29,7 +21,8 @@ descriptionInput.value = descriptionProfile.textContent;
 
 popupForm.addEventListener('submit', function (event) {
   event.preventDefault();
+
   nameProfile.textContent = nameInput.value;
   descriptionProfile.textContent = descriptionInput.value;
-  closePopup(editPopup);
+  editPopup.classList.remove('popup_opened');
 });
