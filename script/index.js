5d04793740d9d62f13fd1574fac4e9809eb0be74
Moreaux19 <basargin19@gmail.com>
2023-07-03 15:41:43 +0300

    Reviewer remarks done

--- script/index.js
+++ script/index.js
@@ -2,14 +2,12 @@ const editPopupButton = document.querySelector('.profile__edit-button');
 const nameProfile = document.querySelector('.profile__name');
 const descriptionProfile = document.querySelector('.profile__description');
 const editPopup = document.querySelector('#popup-profile');
-const closePopupButtonProfile = editPopup.querySelector('#popup-close-button-profile');
 const popupProfileForm = editPopup.querySelector('#popup-profile-form');
 const nameInput = popupProfileForm.querySelector('#name-input');
 const descriptionInput = popupProfileForm.querySelector('#description-input');
 
 const addPopupButton = document.querySelector('.profile__add-button');
 const addPopup = document.querySelector('#popup-element');
-const closePopupButtonEl = addPopup.querySelector('#popup-close-button-element');
 const popupElementForm = addPopup.querySelector('#popup-element-form');
 const imageNameInput = popupElementForm.querySelector('#image-name-input');
 const imageUrlInput = popupElementForm.querySelector('#image-url-input');
@@ -19,47 +17,31 @@ const elements = document.querySelector('.elements');
 const element = template.querySelector('.element');
 
 const imagePopup = document.querySelector('#popup-image');
-const closePopupImageButton = imagePopup.querySelector('#popup-close-button-image');
 const popupFullImage = imagePopup.querySelector('.popup__full-image');
 const popupImageText = imagePopup.querySelector('.popup__image-text');
+const closeButton = document.querySelectorAll('.popup__close-button');
 
-const initialCards = [
-  {
-    name: 'Архыз',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
-  },
-  {
-    name: 'Челябинская область',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
-  },
-  {
-    name: 'Иваново',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
-  },
-  {
-    name: 'Камчатка',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
-  },
-  {
-    name: 'Холмогорский район',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
-  },
-  {
-    name: 'Байкал',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
-  }
-];
+// функция закрытия всех попапов
+closeButton.forEach(button => {
+  const buttonsPopup = button.closest('.popup'); // нашли родителя с нужным классом
+  button.addEventListener('click', () => closePopup(buttonsPopup)); // закрыли попап
+});
+
+// функция открытия попапа
+function openPopup(popup) {
+  popup.classList.add('popup_opened');
+}
+
+// функция закрытия попапа
+function closePopup(popup) {
+  popup.classList.remove('popup_opened');
+}
 
 // открытие окна редактирования профиля
 editPopupButton.addEventListener('click', function () {
   nameInput.value = nameProfile.textContent;
   descriptionInput.value = descriptionProfile.textContent;
-  editPopup.classList.add('popup_opened');
-});
-
-// закрытие окна редактирования профиля
-closePopupButtonProfile.addEventListener('click', function () {
-  editPopup.classList.remove('popup_opened');
+  openPopup(editPopup);
 });
 
 // редактирование профиля
@@ -68,17 +50,12 @@ popupProfileForm.addEventListener('submit', function (event) {
 
   nameProfile.textContent = nameInput.value;
   descriptionProfile.textContent = descriptionInput.value;
-  editPopup.classList.remove('popup_opened');
+  closePopup(editPopup);
 });
 
 // открытие окна добавления изображения
 addPopupButton.addEventListener('click', function () {
-  addPopup.classList.add('popup_opened');
-});
-
-// закрытие окна добавления изображения
-closePopupButtonEl.addEventListener('click', function () {
-  addPopup.classList.remove('popup_opened');
+  openPopup(addPopup);
 });
 
 //создание сетки изображений
@@ -104,16 +81,11 @@ function createElement(value) {
 
   //открытие изображения
   image.addEventListener('click', function () {
-    imagePopup.classList.add('popup_opened');
+    openPopup(imagePopup);
 
     popupFullImage.src = image.src;
     popupFullImage.alt = image.alt;
     popupImageText.textContent = text.textContent;
-  });
-
-  //закрытие изображения
-  closePopupImageButton.addEventListener('click', function () {
-    imagePopup.classList.remove('popup_opened');
   });
 
   return newElement;
@@ -135,5 +107,5 @@ popupElementForm.addEventListener('submit', function (event) {
   event.preventDefault();
   renderElement({ name: imageNameInput.value, link: imageUrlInput.value }, elements);
   popupElementForm.reset();
-  addPopup.classList.remove('popup_opened');
+  closePopup(addPopup);
 });
